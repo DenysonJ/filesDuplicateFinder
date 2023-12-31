ec24cb73bc4d9dc00901d953a743eb45ca6e6d6f
Denyson Grellert <denysonjurgen@gmail.com>
2023-12-31 19:14:25 -0300

    Create the first test

---
+++ tests/test_fileByteCompare.py
@@ -0,0 +1,12 @@
+import unittest
+
+from include.fileByteCompare import validate_file_contents
+
+
+class TestByteCompare(unittest.TestCase):
+    def test_validate_file_contents(self):
+        self.assertTrue(validate_file_contents('fixtures/test1.txt', 'fixtures/test2.txt'))
+        self.assertFalse(validate_file_contents('fixtures/test1.txt', 'fixtures/test3.txt'))
+
+if __name__ == '__main__':
+    unittest.main()
