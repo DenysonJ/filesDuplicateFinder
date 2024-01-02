2bb0aa0b33ce53d4fdd88065b0ee9572eb28a3cd
Denyson Grellert <denysonjurgen@gmail.com>
2024-01-01 23:27:37 -0300

    Create tests for different states

--- tests/test_imageCompare.py
+++ tests/test_imageCompare.py
@@ -5,14 +5,30 @@ from include.imageCompare import ImageCompare
 
 class TestImageCompare(unittest.TestCase):
   def test_image_compare_hard(self):
-    self.assertFalse(ImageCompare('fixtures/sample_640x426.bmp', 'fixtures/sample_1280x853.bmp').image_pixel_differences())
-    self.assertTrue(ImageCompare('fixtures/sample_640x426.bmp', 'fixtures/sample_640x426.bmp').image_pixel_differences())
+    self.assertFalse(ImageCompare(
+                      'fixtures/sample_640x426.bmp',
+                      'fixtures/sample_1280x853.bmp').image_pixel_differences())
+    self.assertTrue(ImageCompare(
+                      'fixtures/sample_640x426.bmp',
+                      'fixtures/sample_640x426.bmp').image_pixel_differences())
     
-  def test_image_compare_soft(self):
-    result = []
-    result.append(ImageCompare('fixtures/sample_640x426.bmp', 'fixtures/sample_1280x853.bmp').image_similarity())
-    result.append(ImageCompare('fixtures/sample_640x426.bmp', 'fixtures/sample_640x426.bmp').image_similarity())
-    self.assertTrue(result[0][0])
-    self.assertTrue(result[1][0])
-    self.assertGreaterEqual(result[0][1], 0.85)
-    self.assertEqual(result[1][1], 1.0)
+  def test_image_compare_soft_equal(self):
+    result = ImageCompare(
+        'fixtures/sample_640x426.bmp',
+        'fixtures/sample_640x426.bmp').image_similarity()
+    self.assertTrue(result[0])
+    self.assertEqual(result[1], 1.0)
+    
+  def test_image_compare_soft_similar(self):
+    result = ImageCompare(
+        'fixtures/sample_640x426.bmp',
+        'fixtures/sample_1280x853.bmp').image_similarity()
+    self.assertTrue(result[0])
+    self.assertGreaterEqual(result[1], 0.85)
+    
+  def test_image_compare_soft_different(self):
+    result = ImageCompare(
+              'fixtures/sample_640x426.bmp',
+              'fixtures/594_900x900.jpg').image_similarity()
+    self.assertFalse(result[0])
+    self.assertLess(result[1], 0.85)
