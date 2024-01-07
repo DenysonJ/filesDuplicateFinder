e61a81c33dcc258fc94b0f942812e4155e869fd5
Denyson Grellert <denysonjurgen@gmail.com>
2024-01-07 12:09:11 -0300

    Fix test names

--- tests/test_videoCompare.py
+++ tests/test_videoCompare.py
@@ -17,26 +17,26 @@ class TestVideoCompare(unittest.TestCase):
                       'fixtures/sample_640x360.mp4',
                       'fixtures/sample_960x400_ocean_with_audio.mov')
       
-  def test_image_compare_hard(self):
+  def test_video_compare_hard(self):
     self.assertFalse(self.similar.compare_videos_hard())
     self.assertTrue(self.equal.compare_videos_hard())
     
-  def test_image_compare_soft_equal(self):
+  def test_video_compare_soft_equal(self):
     result = self.equal.compare_videos_soft()
     self.assertTrue(result[0])
     self.assertEqual(result[1], 1.0)
     
-  def test_image_compare_soft_similar(self):
+  def test_video_compare_soft_similar(self):
     result = self.similar.compare_videos_soft()
     self.assertTrue(result[0])
     self.assertGreaterEqual(result[1], 0.85)
     
-  def test_image_compare_soft_different(self):
+  def test_video_compare_soft_different(self):
     result = self.different.compare_videos_soft()
     self.assertFalse(result[0])
     self.assertLess(result[1], 0.85)
 
-  def test_image_compare_verbose(self):
+  def test_video_compare_verbose(self):
     message = "Videos have different lengths\n"
     m1 = "Video 1: 13.7931 seconds\n"
     m2 = "Video 2: 48.5217 seconds"
@@ -46,7 +46,7 @@ class TestVideoCompare(unittest.TestCase):
     output = sys.stdout.getvalue().strip()
     self.assertEqual(output, message)
   
-  def test_image_compare_verbose_similar(self):
+  def test_video_compare_verbose_similar(self):
     message = "Video similarity (SSIM): 0.9791"
     self.similar.verbose = 1
     self.similar.compare_videos_soft()
