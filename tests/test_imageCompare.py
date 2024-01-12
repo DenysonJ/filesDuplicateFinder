bc8d3c512b13c8d492ac1810af87bad60a011be8
Denyson Grellert <denysonjurgen@gmail.com>
2024-01-11 23:41:08 -0300

    Create new tests to increase code coverage

--- tests/test_imageCompare.py
+++ tests/test_imageCompare.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
 
+from numpy import ndarray
+
 from include.imageCompare import ImageCompare
 
 
@@ -41,3 +43,22 @@ class TestImageCompare(unittest.TestCase):
       verbose=1).image_similarity()
     output = sys.stdout.getvalue().strip()
     self.assertEqual(output, 'Image similarity (SSIM): 0.1777')
+
+  def test_image_reverse_resolution(self):
+    result = ImageCompare(
+      'fixtures/images_sizes/sample-images-05.jpeg',
+      'fixtures/images_sizes/R.jpg').image_similarity()
+    self.assertFalse(result[0])
+
+  def test_show_images(self):
+    result = ImageCompare(
+      'fixtures/sample_640x426.bmp',
+      'fixtures/594_900x900.jpg',
+      show_images=True).image_similarity()
+    
+    self.assertIsInstance(result[2][0], ndarray)
+    self.assertIsInstance(result[2][1], ndarray)
+    self.assertIsInstance(result[2][2], ndarray)
+    self.assertEqual(result[2][0].shape, (426, 640))
+    self.assertEqual(result[2][1].shape, (426, 640, 3))
+    self.assertEqual(result[2][2].shape, (426, 640, 3))
